a67e3448393f0ed6098a3a3c5bfd8003dc912d4d
Fati Idris <fatiidris2012@gmail.com>
2025-08-30 00:11:05 +0100

    chat pages update

--- resources/views/chat/_message.blade.php
+++ resources/views/chat/_message.blade.php
@@ -1,20 +1,24 @@
-<div class="chat-header clearfix">
-     @include('chat._header')
-</div>
-<div class="chat-history">
-    
-     @include('chat._chat')
-</div>
-<div class="chat-message clearfix">
-      <form action="" id="submit_message" method="post" class="mb-0">
-        <input type="text">
-        {{ csrf_field() }}
-             <textarea name="" class="form-control"></textarea>
-        <div class="row">
-            <div class="col-md-6"></div>
-            <div class="col-md-6" style="text-align: right;">
-                <button style="margin-top: 10px;" class="btn btn-primary">Send</button>
+@foreach($getChat as $value)
+    @if($value->sender_id == Auth::user()->id)
+        <li class="clearfix">
+            <div class="message-data text-right">
+                <span class="message-data-time">
+                    {{ Carbon\Carbon::parse($value->created_date)->diffForHumans() }}
+                </span>
+                <img style="height: 40px;" src="{{ $value->getSender->getProfileDirect() }}" alt="avatar">
             </div>
-          </div>
-      </form>
-</div>
+            <div class="message other-message float-right">{!! $value->message !!}</div>
+        </li>
+    @else
+        <li class="clearfix">
+            <div class="message-data">
+                <img style="height: 40px;" src="{{ $value->getSender->getProfileDirect() }}" alt="avatar">
+                <span class="message-data-time">
+                    {{ Carbon\Carbon::parse($value->created_date)->diffForHumans() }}
+                    
+                </span>
+            </div>
+            <div class="message my-message">{!! $value->message !!}</div>
+        </li>
+    @endif
+@endforeach
